a054462963a34964216f80d4847c3e2d4e75e027
GauravYadav <gk54801325@gmail.com>
2022-06-02 10:06:43 +0530

    Charts, Hedaer, Orders, Ecommerce, Employees pages are DONE

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -94,6 +94,18 @@ const Ecommerce = () => {
                                           color="blue"
                                         />
                                    </div>
+                                   <div className="mt-10">
+                                        <Button 
+                                           color="white"
+                                           bgColor="blue"
+                                           text="Download Report"
+                                           borderRadius="10px"
+                                        >
+                                        </Button>
+                                   </div>
+                              </div>
+                              <div>
+                                   <Stacked width="320px" height="360px" />
                               </div>
                          </div>
                     </div>
